8b7c5e85f5f73573682923019f51487a96549f7d
vik_lav <iborn16.04.2015@gmail.com>
2023-10-15 13:50:56 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import csv
+import json
+import psycopg2
+
+conn = psycopg2.connect("dbname=FundsManagementDB user=postgres password=633804735")
+cur = conn.cursor()
+
+'''##########################JSON MANIPULATION##########################'''
+def loadCategories():
+    jsonFile = json.load(open('categories.json'))
+    return jsonFile['categories']
+
+def addInflowCategory(category):
+    jsonFile = json.load(open('categories.json'))
+    jsonFile['categories']['inflow'].append(category)
+    os.remove('categories.json')
+    with open('categories.json', 'w') as f:
+        json.dump(jsonFile, f, indent=4)
+
+def addOutflowCategory(category):
+    jsonFile = json.load(open('categories.json'))
+    jsonFile['categories']['outflow'].append(category)
+    os.remove('categories.json')
+    with open('categories.json', 'w') as f:
+        json.dump(jsonFile, f, indent=4)
+
+'''##########################TRANSACTION MANIPULATION##########################'''
+def getExpensesCSV(filename):
+    expenses = []
+    with open(f"csv/{filename}.csv", 'r') as file:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            expenses.append(row)
+    return expenses[::-1]
+
+def main(filename):
+    expenses = getExpensesCSV(filename)
+    for expense in expenses:
+        date = '{year}-{month}-{day}'.format(year = expense[0].split('/')[2], month = expense[0].split('/')[1], day = expense[0].split('/')[0]) 
+        origin = expense[4]
+        if expense[5]:
+            nature = True
+            amount = float(expense[5])
+        else:
+            nature = False
+            amount = float(expense[6])
+        if nature:
+            categories = loadCategories()['outflow']
+            #print('Date         Source          Expenditure     Amount')
+            print(f'{date}    {origin}    {nature}    {amount}')
+            for category in categories:
+                print(f'{categories.index(category)+1} - {category}')
+            print(f'{len(categories)+1} - Add another category')
+            choice = int(input('What?:> '))
+            if choice <= len(categories):
+                category = categories[choice-1]
+                cur.execute('INSERT INTO AccountBalanceManagement (amount, date, source, description, expenditure) VALUES (%s,%s,%s,%s,%s)', (amount, date, category, origin, True))
+                conn.commit()
+            elif choice == len(categories)+1:
+                newCategory = input('New category:> ')
+                addOutflowCategory(newCategory)
+                cur.execute('INSERT INTO AccountBalanceManagement (amount, date, source, description, expenditure) VALUES (%s,%s,%s,%s,%s)', (amount, date, newCategory, origin, True))
+                conn.commit()
+        else:
+            categories = loadCategories()['inflow']
+            print(f'{date}    {origin}    {nature}    {amount}')
+            for category in categories:
+                print(f'{categories.index(category)+1} - {category}')
+            print(f'{len(categories)+1} - Add another category')
+            choice = int(input('What?:> '))
+            if choice <= len(categories):
+                category = categories[choice-1]
+                cur.execute('INSERT INTO AccountBalanceManagement (amount, date, source, description, expenditure) VALUES (%s,%s,%s,%s,%s)', (amount, date, category, origin, False))
+                conn.commit()
+            elif choice == len(categories)+1:
+                newCategory = input('New category:> ')
+                addInflowCategory(newCategory)
+                cur.execute('INSERT INTO AccountBalanceManagement (amount, date, source, description, expenditure) VALUES (%s,%s,%s,%s,%s)', (amount, date, newCategory, origin, False))
+                conn.commit()
+
+
+if __name__ == '__main__':
+    main('transactions5')
---
+++ queries.sql
@@ -0,0 +1,27 @@
+CREATE EXTENSION tablefunc;
+SELECT * 
+FROM crosstab(
+	'SELECT SUM(amount), source, CONCAT(date_part("Month", date), "/", date_part("year", date))
+	FROM public."AccountBalanceManagement"
+	WHERE expenditure
+	GROUP BY source, 3;'
+) AS ct (date text, "Bills" int, "Transport" int, "Groceries" int, "Food" int, "Presents" int, "Entertainment" int, "Education" int, "Other" int);
+
+SELECT SUM(amount), source, TO_CHAR(date, 'YYYY-MM')
+FROM public."AccountBalanceManagement"
+WHERE expenditure
+GROUP BY source, 3
+ORDER BY 3;
+
+select distinct trim(both ' ' from source) from "AccountBalanceManagement" where expenditure order by 1
+
+select distinct to_char(date, 'Month YYYY') from "AccountBalanceManagement"
+
+SELECT 
+  SUM(amount), 
+  trim(both ' ' from source), 
+  TO_CHAR(date, 'YYYY-MM') AS "Month"
+FROM "AccountBalanceManagement" 
+WHERE expenditure = true
+GROUP BY 2,3
+ORDER BY 3,2;
